b600edc1e7258a82f3ed1c6e6971ce51b99a4b2a
LeinaD09 <dfirebluecm@gmail.com>
2025-08-02 12:18:35 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, send_file
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '¿Aceptas los términos y condiciones?'
+    return send_file('pagina-web.html')
 
 if __name__ == '__main__':
     app.run()
